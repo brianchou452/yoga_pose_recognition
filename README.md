27234b06307583dc7f30032f9ead537931170b9b
海獺 <brianchou452@gmail.com>
2024-12-21 17:09:20 +0800

    feat: 新增electron app

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # 瑜伽姿勢辨識
+
+## Usage
+
+### Create an App
+
+```bash
+# with npx
+$ npx create-nextron-app my-app --example basic-lang-typescript
+
+# with yarn
+$ yarn create nextron-app my-app --example basic-lang-typescript
+
+# with pnpm
+$ pnpm dlx create-nextron-app my-app --example basic-lang-typescript
+```
+
+### Install Dependencies
+
+```bash
+$ cd my-app
+
+# using yarn or npm
+$ yarn (or `npm install`)
+
+# using pnpm
+$ pnpm install --shamefully-hoist
+```
+
+### Use it
+
+```bash
+# development mode
+$ yarn dev (or `npm run dev` or `pnpm run dev`)
+
+# production build
+$ yarn build (or `npm run build` or `pnpm run build`)
+```
---
+++ main/background.ts
@@ -0,0 +1,62 @@
+import { ChildProcess, spawn } from 'child_process'
+import { app, ipcMain } from 'electron'
+import serve from 'electron-serve'
+import * as os from 'os'
+import * as path from 'path'
+import { createWindow } from './helpers'
+
+const isProd = process.env.NODE_ENV === 'production'
+let pythonProcess: ChildProcess | null = null
+
+if (isProd) {
+  serve({ directory: 'app' })
+} else {
+  app.setPath('userData', `${app.getPath('userData')} (development)`)
+}
+
+; (async () => {
+  await app.whenReady()
+
+  const mainWindow = createWindow('main', {
+    width: 1000,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  })
+
+  // 動態設置 Python 解釋器的路徑
+  const pythonDir = os.platform() === 'win32' ? 'Scripts' : 'bin';
+  const pythonPathDev = os.platform() === 'win32' ? '.venv\\Scripts\\python.exe' : '.venv/bin/python';
+  const pythonPath = isProd
+    ? path.join(process.resourcesPath, '.venv', pythonDir, 'python')
+    : pythonPathDev;
+
+  // 啟動 Python 腳本
+  pythonProcess = spawn(pythonPath, ['-m', 'yoga_pose_recognition'])
+
+  mainWindow.on('closed', () => {
+    // 結束 Python 進程
+    if (pythonProcess) {
+      pythonProcess.kill()
+      pythonProcess = null
+    }
+    app.quit()
+  })
+
+  if (isProd) {
+    await mainWindow.loadURL('app://./home')
+  } else {
+    const port = process.argv[2]
+    await mainWindow.loadURL(`http://localhost:${port}/home`)
+    mainWindow.webContents.openDevTools()
+  }
+})()
+
+app.on('window-all-closed', () => {
+  app.quit()
+})
+
+ipcMain.on('message', async (event, arg) => {
+  event.reply('message', `${arg} World!`)
+})
